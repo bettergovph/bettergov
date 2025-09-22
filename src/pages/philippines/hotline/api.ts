32de3b04fc900cd51a209650074128fc685d1b38
IamIsthill <bercasiocharles14@gmail.com>
2025-09-22 22:20:23 +0800

    refactor: remove unnecessary typings and simplify hooks
    - Add user friendly error

--- src/pages/philippines/hotline/api.ts
+++ src/pages/philippines/hotline/api.ts
@@ -1,70 +1,60 @@
 import { toast } from 'sonner';
 import { useState } from 'react';
 
-const useMailerApi = () => {
-  // const api = axios.create({
-  //   baseURL: import.meta.env.VITE_MAILER_API,
-  // });
-  const api = async <T>(path: string, options: RequestInit) => {
-    const baseUrl = import.meta.env.VITE_MAILER_API as string;
-    const res = await fetch(baseUrl + path, {
-      headers: {
-        'Content-Type': 'application/json',
-        ...(options.headers || {}),
-      },
-      ...options,
-    });
+const api = async <T>(path: string, options: RequestInit) => {
+  const baseUrl = import.meta.env.VITE_MAILER_API as string;
+  const res = await fetch(baseUrl + path, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...(options.headers || {}),
+    },
+    ...options,
+  });
 
-    if (!res.ok) {
-      const errorText = await res.text();
-      throw new Error(errorText || `Request failed with status ${res.status}`);
-    }
-    const data = await res.text();
+  if (!res.ok) {
+    const errorText = await res.text();
+    throw new Error(errorText || `Request failed with status ${res.status}`);
+  }
+  const data = await res.text();
 
-    return data ? (JSON.parse(data) as T) : {};
-  };
-
-  /**
-   * Use this hook for sending an outdated hotline report
-   */
-  const useSendOutdatedHotlineReport = () => {
-    const [isError, setError] = useState<boolean>(false);
-    const [isLoading, setLoading] = useState(false);
-
-    const sendOutdatedHotlineReport = async (payload: {
-      organization: string;
-      outdated_hotline: string;
-      updated_hotline: string;
-    }) => {
-      try {
-        setLoading(true);
-        const response = await api<{ message: string }>('/hotlines/outdated', {
-          method: 'post',
-          body: JSON.stringify(payload),
-        });
-        toast.success('Report sent', {
-          description: 'Your report was successfully sent',
-        });
-        return response;
-      } catch (error) {
-        setError(true);
-        toast.error('Failed to send', {
-          description: 'Your report was not sent',
-        });
-        console.error('Failed to send hotline report:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    return {
-      isError,
-      isLoading,
-      sendOutdatedHotlineReport,
-    };
-  };
-
-  return { api, useSendOutdatedHotlineReport };
+  return data ? (JSON.parse(data) as T) : {};
 };
 
-export { useMailerApi };
+const useSendOutdatedHotlineReport = () => {
+  const [isError, setError] = useState<boolean>(false);
+  const [isLoading, setLoading] = useState(false);
+
+  const sendOutdatedHotlineReport = async (payload: {
+    organization: string;
+    outdated_hotline: string;
+    updated_hotline: string;
+  }) => {
+    try {
+      setLoading(true);
+      const response = await api<{ message: string }>('/hotlines/outdated', {
+        method: 'post',
+        body: JSON.stringify(payload),
+      });
+      toast.success('Report sent', {
+        description: 'Your report was successfully sent',
+      });
+      return response;
+    } catch (error) {
+      setError(true);
+      toast.error('Failed to send', {
+        description: 'Your report was not sent',
+      });
+      console.error('Failed to send hotline report:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return {
+    isError,
+    isLoading,
+    sendOutdatedHotlineReport,
+  };
+};
+
+export { useSendOutdatedHotlineReport };
