fb336e897946ed403e507f9e6712700d74a0f41b
Waffen Sultan <waffen.sultan@gmail.com>
2025-09-28 17:32:08 +0800

    chore: remove '@functions/' alias

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@functions': path.resolve(__dirname, '@./functions/*'),
     },
   },
 });
