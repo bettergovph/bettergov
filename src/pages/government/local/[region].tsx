86352c9dfdffbe1b55a4681804c77946db739e45
Ban <203548283+aybangueco@users.noreply.github.com>
2025-10-06 10:54:25 +0800

    Update src/pages/government/local/[region].tsx
    Co-authored-by: Nicu Listana <niculistana@gmail.com>

--- src/pages/government/local/[region].tsx
+++ src/pages/government/local/[region].tsx
@@ -215,7 +215,17 @@ export default function RegionalLGUPage() {
                           {unit.mayor?.name || 'No Mayor Listed'}
                         </p>
                         <p className='text-xs text-gray-800'>Mayor</p>
-                        {unit.mayor?.contact ||
+                        {unit.mayor?.contact && (
+                          <div className='mt-1'>
+                            <CardContactInfo
+                              contact={{
+                                phone: unit.mayor.contact,
+                                email: unit.mayor.email,
+                              }}
+                              compact
+                            />
+                          </div>
+                        )}
                           (unit.mayor?.email && (
                             <div className='mt-1'>
                               <CardContactInfo
