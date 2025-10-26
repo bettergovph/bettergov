ef3b1318b278e114e49068fa42ad7ae7971d53ee
Lord Marin <lordgpm@gmail.com>
2025-10-26 09:17:50 +0800

    feat: add helper for intrapolation of repeated strings

---
+++ src/lib/seoTemplates.ts
@@ -0,0 +1,7 @@
+export function formatStandardTitle(subject: string): string {
+  return `${subject} in Philippines | BetterGov.ph`;
+}
+
+export function formatStandardDescription(subject: string): string {
+  return `Find important information about ${subject} through BetterGov.ph, the Philippines’ civic information portal.`;
+}
