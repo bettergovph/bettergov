eaddc59c4159c1dbc6e66cf3489e0b6855ced5c5
decimozs <marlonadiguemartin548@gmail.com>
2025-12-26 15:02:49 +0800

    feat: implement PWA support using vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,66 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     outDir: 'dist',
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        navigateFallback: '/index.html',
+        navigateFallbackAllowlist: [/^\/$/],
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        name: 'BetterGov.ph',
+        short_name: 'BetterGov.ph',
+        icons: [
+          {
+            src: '/pwa/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/pwa/pwa-maskable-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/pwa/pwa-maskable-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        start_url: '/',
+        lang: 'en-US',
+        display: 'fullscreen',
+        background_color: '#FFFFFF',
+        theme_color: '#FFFFFF',
+        description:
+          'A community-led initiative to create a better and more usable Philippine national government website.',
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
